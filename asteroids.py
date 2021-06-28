7293fdd3198af1458df238ace03dc59e0c30efcd
Leandro Santiago da Silva <lsds.snf20@uea.edu.br>
2021-06-27 23:03:25 -0400

    Add asteroids class

---
+++ asteroids.py
@@ -0,0 +1,61 @@
+from constants import *
+
+import random
+import math
+import pygame
+
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+# Create class asteroid
+class Asteroid:
+    def __init__(self, x, y, t):
+        self.x = x
+        self.y = y
+        if t == "Large":
+            self.size = 30
+        elif t == "Normal":
+            self.size = 20
+        else:
+            self.size = 10
+        self.t = t
+
+        # Make random speed and direction
+        self.speed = random.uniform(1, (40 - self.size) * 4 / 15)
+        self.dir = random.randrange(0, 360) * math.pi / 180
+
+        # Make random asteroid sprites
+        full_circle = random.uniform(18, 36)
+        dist = random.uniform(self.size / 2, self.size)
+        self.vertices = []
+        while full_circle < 360:
+            self.vertices.append([dist, full_circle])
+            dist = random.uniform(self.size / 2, self.size)
+            full_circle += random.uniform(18, 36)
+
+    def update_asteroid(self):
+        # Move asteroid
+        self.x += self.speed * math.cos(self.dir)
+        self.y += self.speed * math.sin(self.dir)
+
+        # Check for wrapping
+        if self.x > WIDTH:
+            self.x = 0
+        elif self.x < 0:
+            self.x = WIDTH
+        elif self.y > HEIGHT:
+            self.y = 0
+        elif self.y < 0:
+            self.y = HEIGHT
+
+        # Draw asteroid
+        for v in range(len(self.vertices)):
+            if v == len(self.vertices) - 1:
+                next_v = self.vertices[0]
+            else:
+                next_v = self.vertices[v + 1]
+            this_v = self.vertices[v]
+            pygame.draw.line(gameDisplay, WHITE, (self.x + this_v[0] * math.cos(this_v[1] * math.pi / 180),
+                                                  self.y + this_v[0] * math.sin(this_v[1] * math.pi / 180)),
+                             (self.x + next_v[0] * math.cos(next_v[1] * math.pi / 180),
+                              self.y + next_v[0] * math.sin(next_v[1] * math.pi / 180)))
